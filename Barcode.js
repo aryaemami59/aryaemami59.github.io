45c34cdc764e19e5edeac4142cc90deacab8c7d8
arya <aryaemami59@yahoo.com>
2021-10-29 20:03:25 -0500

    Update Barcode.js

--- Barcode.js
+++ Barcode.js
@@ -4135,7 +4135,7 @@ function printBarcode (barcode) {
         $(event.target).printThis({
             printDelay: 50,
             importCSS: false,
-            loadCSS: 'Barcode Project/printingBarcodes.css',
+            loadCSS: 'aryaemami59.github.io/printingBarcodes.css',
             // loadCSS: 'aryaemami59.github.io/printingBarcodes.css',
             });
     })
